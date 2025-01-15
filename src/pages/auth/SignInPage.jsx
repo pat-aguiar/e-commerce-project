fa9bc8d8b45f3bba853e6a72c0733682d6221cdf
Patricia Aguiar <paguiar.eng@gmail.com>
2025-01-14 22:19:41 -0300

    initial setup

---
+++ src/pages/auth/SignInPage.jsx
@@ -0,0 +1,10 @@
+const SignInPage = () => {
+  return (
+    <div className="font-lato text-blue-500 ">
+      sign in
+      <i className="fa-solid fa-face-smile"></i>
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ src/pages/auth/SignUpPage.jsx
@@ -0,0 +1,10 @@
+const SignUpPage = () => {
+  return (
+    <div className="font-playfair text-green-500">
+      sign up
+      <i className="fa-solid fa-right-to-bracket"></i>
+    </div>
+  );
+};
+
+export default SignUpPage;
