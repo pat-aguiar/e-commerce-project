5a2c55d124ceca5993c83b6f11cfc203b5054c6e
Patricia Aguiar <paguiar.eng@gmail.com>
2025-01-17 19:54:37 -0300

    back-end api integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignInPage from "./pages/auth/SignInPage";
 import SignUpPage from "./pages/auth/SignUpPage";
+import apiFetch from "./services/apiFetch";
+
+// const response = await apiFetch("GET", "/api-key/info");
+// console.log(response.status);
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage/>}/>
+        <Route path="/sign-up" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
